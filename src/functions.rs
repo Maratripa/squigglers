7049c8dd6d07d259551110423b9d82f2bb4cc3e8
Martin Atria Paublo <maratripa@gmail.com>
2023-08-15 23:54:55 -0400

    feat: to funtion

--- src/functions.rs
+++ src/functions.rs
@@ -1,5 +1,16 @@
-pub fn to(x: i64, y: i64, credibility: Option<u8>) -> Dist {
-    if x > 0 {
-        norm(x, y, credibility)
+use crate::{
+    distributions::DistNode,
+    interface::{LogNormal, Normal},
+};
+
+/// Initialize a distribution from `x` to `y`.
+///
+/// The distribution will be lognormal by default, unless `x` is less than or equal to 0,
+/// in which case it will become a normal distribution.
+pub fn to(x: f64, y: f64, credibility: f64) -> DistNode {
+    if x > 0.0 {
+        LogNormal::from_range(x, y, credibility)
+    } else {
+        Normal::from_range(x, y, credibility)
     }
 }
