62751ad83685a4559cd15a9a61df83e1ebf11ba3
Martin Atria Paublo <maratripa@gmail.com>
2023-08-16 21:10:58 -0400

    fix: lognormal creation interface parameters

--- src/interface.rs
+++ src/interface.rs
@@ -9,13 +9,6 @@ use crate::utils::{ppf, square};
 
 pub struct Normal {}
 impl Normal {
-    pub fn new(mean: f64, std_dev: f64) -> DistNode {
-        // TODO: Handle nan cases for mean and std_dev
-        DistNode::Continuous(ContinuousDist::Normal {
-            dist: NormalDist::new(mean, std_dev.abs()).unwrap(),
-        })
-    }
-
     pub fn from_mean(mean: f64, std_dev: f64) -> DistNode {
         DistNode::Continuous(ContinuousDist::Normal {
             dist: NormalDist::new(mean, std_dev.abs()).unwrap(),
@@ -34,12 +27,6 @@ impl Normal {
 
 pub struct LogNormal {}
 impl LogNormal {
-    pub fn new(mean: f64, std_dev: f64) -> DistNode {
-        DistNode::Continuous(ContinuousDist::LogNormal {
-            dist: LogNormalDist::new(mean, std_dev).unwrap(),
-        })
-    }
-
     pub fn from_loc(location: f64, scale: f64) -> DistNode {
         DistNode::Continuous(ContinuousDist::LogNormal {
             dist: LogNormalDist::new(location, scale).unwrap(),
@@ -47,20 +34,20 @@ impl LogNormal {
     }
 
     pub fn from_mean(mean: f64, std_dev: f64) -> DistNode {
-        let location = (square(mean) / (square(mean) + square(std_dev)).sqrt()).ln();
+        let location = 2. * mean.ln() - 0.5 * (square(std_dev) + square(mean)).ln();
 
-        let scale = (1. + square(std_dev) / square(mean)).ln() / 2.;
+        let scale = (1. + square(std_dev) / square(mean)).ln().sqrt();
 
         Self::from_loc(location, scale)
     }
 
     pub fn from_range(x: f64, y: f64, credibility: f64) -> DistNode {
-        let mean = (x.ln() + y.ln()) / 2.;
-        let cdf_value = 0.5 * (1. + (credibility / 100.));
-        let normed_sigma = ppf(cdf_value);
-        let std_dev = (y.ln() - mean) / normed_sigma;
+        let location = (x.ln() + y.ln()) / 2.;
+        let cdf_value = (1. + (credibility / 100.)) / 2.;
+        let normed_scale = ppf(cdf_value);
+        let scale = (y.ln() - location) / normed_scale;
 
-        Self::from_mean(mean, std_dev)
+        Self::from_loc(location, scale)
     }
 }
 
